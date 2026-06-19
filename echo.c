adb51149c272ba701766af1152f1bee01f7084a3
James Thai <jamesthai79@gmail.com>
2026-06-18 22:14:36 -0700

    fixed logic bug with switch statement

--- echo.c
+++ echo.c
@@ -10,30 +10,40 @@ int process_char(char **c) {
   if (**c != '\\') {
     value = **c;
     (*c) = (*c) + 1;
+    return value;
   }
-  return value;
   (*c) = (*c) + 1;
   switch (**c) {
   case '\\':
     value = '\\';
+    break;
   case 'a':
     value = '\a';
+    break;
   case 'b':
     value = '\b';
+    break;
   case 'c':
     value = -1;
+    break;
   case 'e':
     value = '\e';
+    break;
   case 'f':
     value = '\f';
+    break;
   case 'n':
     value = '\n';
+    break;
   case 'r':
     value = '\r';
+    break;
   case 't':
     value = '\t';
+    break;
   case 'v':
     value = '\v';
+    break;
   }
   (*c) = (*c) + 1;
   return value;
@@ -44,14 +54,13 @@ int main(int argc, char **argv) {
   char *buffer = (char *)malloc(size);
   int opt;
   size_t index = 0;
-  char output = 0;
   while ((opt = getopt(argc, argv, "neE")) != -1) {
     switch (opt) {
     case 'n':
       for (; optind < argc; optind++) {
         char *arg = argv[optind];
         while (*arg != '\0') {
-          char output = (char)process_char(&arg);
+          int output = process_char(&arg);
           if (output == -1) {
             break;
           }
