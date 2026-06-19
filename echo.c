108d17d71f951fca7f97045675e36a5907ae0bdb
James Thai <jamesthai79@gmail.com>
2026-06-18 21:33:37 -0700

    added initial processing of escape characters to echo

--- echo.c
+++ echo.c
@@ -1,19 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #define BUFFER_SIZE 1024
+
+int process_char(char **c) {
+  int value;
+  if (**c != '\\') {
+    value = **c;
+    (*c) = (*c) + 1;
+  }
+  return value;
+  (*c) = (*c) + 1;
+  switch (**c) {
+  case '\\':
+    value = '\\';
+  case 'a':
+    value = '\a';
+  case 'b':
+    value = '\b';
+  case 'c':
+    value = -1;
+  case 'e':
+    value = '\e';
+  case 'f':
+    value = '\f';
+  case 'n':
+    value = '\n';
+  case 'r':
+    value = '\r';
+  case 't':
+    value = '\t';
+  case 'v':
+    value = '\v';
+  }
+  (*c) = (*c) + 1;
+  return value;
+}
 
 int main(int argc, char **argv) {
   int size = BUFFER_SIZE;
   char *buffer = (char *)malloc(size);
-  int c;
+  int opt;
   size_t index = 0;
-  if (argc >= 2) {
-    for (int i = 1; i < argc; i++) {
-      printf("%s ", argv[i]);
+  char output = 0;
+  while ((opt = getopt(argc, argv, "neE")) != -1) {
+    switch (opt) {
+    case 'n':
+      for (; optind < argc; optind++) {
+        char *arg = argv[optind];
+        while (*arg != '\0') {
+          char output = (char)process_char(&arg);
+          if (output == -1) {
+            break;
+          }
+          printf("%c", output);
+        }
+      }
+      break;
+    case 'e':
+
+      printf("Printed out e\n");
+      break;
+    case 'E':
+
+      printf("Printed out E\n");
+      break;
+
+    default:
+
+      break;
     }
   }
-  printf("\n");
   free(buffer);
 
   return 0;
