a5167825a02887165b749c5cc8ec10535504aab1
James Thai <jamesthai79@gmail.com>
2026-06-19 01:03:31 -0700

    implemented and fixed all arguments, TODO: octal and hexadecimal

--- echo.c
+++ echo.c
@@ -4,11 +4,12 @@
 #include <string.h>
 #include <unistd.h>
 
-#define BUFFER_SIZE 1024
+#define FORMATTED true
+#define UNFORMATTED false
 
-int process_char(char **c) {
+int process_char(char **c, bool formatted) {
   int value;
-  if (**c != '\\') {
+  if (**c != '\\' || !formatted) {
     value = **c;
     (*c) = (*c) + 1;
     return value;
@@ -49,69 +50,78 @@ int process_char(char **c) {
   (*c) = (*c) + 1;
   return value;
 }
-void print_input(int argc, char ***argv) {
+
+void print(char **arg, bool formatted) {
+  while (**arg != '\0') {
+    int output = process_char(arg, formatted);
+    if (output == -1) {
+      break;
+    }
+    printf("%c", output);
+  }
+}
+
+void print_input(int argc, char ***argv, bool formatted) {
   if (optind == 1)
     return;
   for (; optind < argc; optind++) {
     char *arg = (*argv)[optind];
-    while (*arg != '\0') {
-      int output = process_char(&arg);
-      if (output == -1) {
-        break;
-      }
-      printf("%c", output);
-    }
+    print(&arg, formatted);
   }
 }
 int main(int argc, char **argv) {
   int opt;
   opterr = 0;
-  bool entered = false;
-  while ((opt = getopt(argc, argv, "neE")) != -1) {
+  bool invalid = false;
+  bool n_argument = false;
+  while ((opt = getopt(argc, argv, "+neE")) != -1) {
+    if (opt == 'n')
+      n_argument = true;
     switch (opt) {
     case '?':
-      if (entered)
+      if (invalid)
         break;
       char *arg = argv[1];
-      while (*arg != '\0') {
-        int output = process_char(&arg);
-        if (output == -1) {
-          break;
-        }
-        printf("%c", output);
-      }
+      print(&arg, FORMATTED);
       printf(" ");
       for (size_t i = 2; i < argc; i++) {
         arg = (argv)[i];
-        while (*arg != '\0') {
-          int output = process_char(&arg);
-          if (output == -1) {
-            break;
-          }
-          printf("%c", output);
-        }
+        print(&arg, FORMATTED);
       }
       printf("\n");
-      entered = true;
+      invalid = true;
       break;
     case 'n':
-      if (!entered)
-        print_input(argc, &argv);
+      if (!invalid)
+        print_input(argc, &argv, FORMATTED);
       break;
     case 'e':
-
-      printf("Printed out e\n");
+      if (!invalid)
+        print_input(argc, &argv, FORMATTED);
       break;
     case 'E':
-
-      printf("Printed out E\n");
+      if (!invalid)
+        print_input(argc, &argv, UNFORMATTED);
       break;
 
     default:
-
+      if (!invalid)
+        print_input(argc, &argv, FORMATTED);
       break;
     }
   }
+
+  if (optind == 1) {
+    char *arg = argv[optind];
+    print(&arg, FORMATTED);
+    for (optind++; optind < argc; optind++) {
+      arg = argv[optind];
+      printf(" ");
+      print(&arg, FORMATTED);
+    }
+  }
+  if (!n_argument && !invalid)
+    printf("\n");
 
   return 0;
 }
