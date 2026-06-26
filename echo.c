ceff950e3a25f09fb921a2eb88c25524a1d30de3
James Thai <jamesthai79@gmail.com>
2026-06-26 00:30:38 -0700

    added new argument processing, refactored code, TODO: hex and octal escape sequences

--- echo.c
+++ echo.c
@@ -1,21 +1,14 @@
 #include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 
-#define FORMATTED true
-#define UNFORMATTED false
+bool newline_argument = false;
+bool escape_argument = true;
+int index_argc = 1;
 
-int process_char(char **c, bool formatted) {
+int process_switch_statement(char c) {
   int value;
-  if (**c != '\\' || !formatted) {
-    value = **c;
-    (*c) = (*c) + 1;
-    return value;
-  }
-  (*c) = (*c) + 1;
-  switch (**c) {
+  switch (c) {
   case '\\':
     value = '\\';
     break;
@@ -47,80 +40,87 @@ int process_char(char **c, bool formatted) {
     value = '\v';
     break;
   }
+  return value;
+}
+int process_char(char **c) {
+  int value = **c;
+  // If it's not formatted, doesn't matter what character it is, return
+  if (!escape_argument) {
+    (*c) = (*c) + 1;
+    return value;
+  }
+  // If it's not a backslash, don't care, return
+  if (value != '\\') {
+    (*c) = (*c) + 1;
+    return value;
+  }
+
+  (*c) = (*c) + 1;
+  // It is formatted AND a backslash
+  // Parse which escape character
+  value = process_switch_statement(**c);
+
+  // Advance string after finishing parsing
   (*c) = (*c) + 1;
   return value;
 }
 
-void print(char **arg, bool formatted) {
-  while (**arg != '\0') {
-    int output = process_char(arg, formatted);
+void print(char *arg) {
+  while (*arg != '\0') {
+    int output = process_char(&arg);
     if (output == -1) {
+      newline_argument = true;
       break;
     }
     printf("%c", output);
   }
 }
 
-void print_input(int argc, char ***argv, bool formatted) {
-  if (optind == 1)
-    return;
-  for (; optind < argc; optind++) {
-    char *arg = (*argv)[optind];
-    print(&arg, formatted);
+void print_input(int argc, char **argv) {
+  for (; index_argc < argc; index_argc++) {
+    char *arg = argv[index_argc];
+    print(arg);
+    if (index_argc < argc - 1)
+      printf(" ");
+  }
+}
+
+void processArguments(int argc, char **argv) {
+  bool invalid_args = false;
+  for (int i = index_argc; i < argc; i++) {
+    if (argv[i][0] != '-') {
+      index_argc = i;
+      break;
+    }
+    char *ptr = argv[i] + 1;
+    while (*ptr != '\0') {
+      if (*ptr == 'n' || *ptr == 'e' || *ptr == 'E') {
+        switch (*ptr) {
+        case 'n':
+          newline_argument = true;
+          break;
+        case 'e':
+          escape_argument = true;
+          break;
+        case 'E':
+          escape_argument = false;
+          break;
+        }
+        ptr++;
+      } else {
+        invalid_args = true;
+        index_argc = i;
+        break;
+      }
+    }
+    if (invalid_args)
+      break;
   }
 }
 int main(int argc, char **argv) {
-  int opt;
-  opterr = 0;
-  bool invalid = false;
-  bool n_argument = false;
-  while ((opt = getopt(argc, argv, "+neE")) != -1) {
-    if (opt == 'n')
-      n_argument = true;
-    switch (opt) {
-    case '?':
-      if (invalid)
-        break;
-      char *arg = argv[1];
-      print(&arg, FORMATTED);
-      printf(" ");
-      for (size_t i = 2; i < argc; i++) {
-        arg = (argv)[i];
-        print(&arg, FORMATTED);
-      }
-      printf("\n");
-      invalid = true;
-      break;
-    case 'n':
-      if (!invalid)
-        print_input(argc, &argv, FORMATTED);
-      break;
-    case 'e':
-      if (!invalid)
-        print_input(argc, &argv, FORMATTED);
-      break;
-    case 'E':
-      if (!invalid)
-        print_input(argc, &argv, UNFORMATTED);
-      break;
-
-    default:
-      if (!invalid)
-        print_input(argc, &argv, FORMATTED);
-      break;
-    }
-  }
-
-  if (optind == 1) {
-    char *arg = argv[optind];
-    print(&arg, FORMATTED);
-    for (optind++; optind < argc; optind++) {
-      arg = argv[optind];
-      printf(" ");
-      print(&arg, FORMATTED);
-    }
-  }
-  if (!n_argument && !invalid)
+  processArguments(argc, argv);
+  print_input(argc, argv);
+  if (!newline_argument)
     printf("\n");
 
   return 0;
