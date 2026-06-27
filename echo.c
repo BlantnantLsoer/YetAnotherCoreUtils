af63c77ff13f2163debb216d047f48f0a7086ecd
James Thai <jamesthai79@gmail.com>
2026-06-27 15:40:37 -0700

    last update: fixed \c escape sequence, refactored code

--- echo.c
+++ echo.c
@@ -2,7 +2,6 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
 bool newline_argument = false;
 bool escape_argument = true;
@@ -18,55 +17,62 @@ int to_hex(char c) {
   return -1;
 }
 
+int process_hex(char **c) {
+  // Process hex
+  char character = *(*c + 1);
+  char character_two = *(*c + 2);
+  if (!isxdigit(character))
+    return '\0';
+  if (!isxdigit(character_two)) {
+    *c = *c + 1;
+    return to_hex(character);
+  }
+  *c = *c + 2;
+  char buf[3];
+  buf[2] = '\0';
+  buf[0] = character;
+  buf[1] = character_two;
+  return strtol(buf, NULL, 16);
+}
+
+int process_octal(char **c) {
+  char buf[4];
+  char character = *(*c + 1);
+  char character_two = *(*c + 2);
+  char character_three = *(*c + 3);
+  if (!((character - '0' >= 0 && character - '0' <= 7))) {
+    return '\0';
+  }
+  if (!((character_two - '0' >= 0 && character_two - '0' <= 7))) {
+    *c = *c + 1;
+    return character - '0';
+  }
+  if (!((character_three - '0' >= 0 && character_three - '0' <= 7))) {
+    *c = *c + 2;
+    buf[2] = '\0';
+    buf[0] = character;
+    buf[1] = character_two;
+    return strtol(buf, NULL, 8);
+  }
+  *c = *c + 3;
+  buf[3] = '\0';
+  buf[0] = character;
+  buf[1] = character_two;
+  buf[2] = character_three;
+  return strtol(buf, NULL, 8);
+}
+
 int process_switch_statement(char **c) {
   int value;
   char character = **c;
 
-  // Process hex
   if (character == 'x') {
-    character = *(*c + 1);
-    char character_two = *(*c + 2);
-    if (!isxdigit(character))
-      return '\0';
-    if (!isxdigit(character_two)) {
-      *c = *c + 1;
-      return to_hex(character);
-    }
-    *c = *c + 2;
-    char buf[3];
-    buf[2] = '\0';
-    buf[0] = character;
-    buf[1] = character_two;
-    return strtol(buf, NULL, 16);
+    return process_hex(c);
   }
-
-  // Process octal
   if (character == '0') {
-    char buf[4];
-    character = *(*c + 1);
-    char character_two = *(*c + 2);
-    char character_three = *(*c + 3);
-    if (!((character - '0' >= 0 && character - '0' <= 7))) {
-      return '\0';
-    }
-    if (!((character_two - '0' >= 0 && character_two - '0' <= 7))) {
-      *c = *c + 1;
-      return character - '0';
-    }
-    if (!((character_three - '0' >= 0 && character_three - '0' <= 7))) {
-      *c = *c + 2;
-      buf[2] = '\0';
-      buf[0] = character;
-      buf[1] = character_two;
-      return strtol(buf, NULL, 8);
-    }
-    *c = *c + 3;
-    buf[3] = '\0';
-    buf[0] = character;
-    buf[1] = character_two;
-    buf[2] = character_three;
-    return strtol(buf, NULL, 8);
+    return process_octal(c);
   }
+  // Process octal
   switch (character) {
   case '\\':
     value = '\\';
@@ -78,7 +84,7 @@ int process_switch_statement(char **c) {
     value = '\b';
     break;
   case 'c':
-    value = -1;
+    exit(EXIT_SUCCESS);
     break;
   case 'e':
     value = '\e';
@@ -98,6 +104,8 @@ int process_switch_statement(char **c) {
   case 'v':
     value = '\v';
     break;
+  default:
+    return -2;
   }
   return value;
 }
@@ -118,6 +126,10 @@ int process_char(char **c) {
   // It is formatted AND a backslash
   // Parse which escape character
   value = process_switch_statement(c);
+  if (value == -2) {
+    value = *(*c - 1);
+    *c = *c - 1;
+  }
   // Advance string after finishing parsing
   (*c) = (*c) + 1;
   return value;
@@ -126,10 +138,6 @@ int process_char(char **c) {
 void print(char *arg) {
   while (*arg != '\0') {
     int output = process_char(&arg);
-    if (output == -1) {
-      newline_argument = true;
-      break;
-    }
     printf("%c", output);
   }
 }
@@ -180,6 +188,5 @@ int main(int argc, char **argv) {
   print_input(argc, argv);
   if (!newline_argument)
     printf("\n");
-
   return 0;
 }
