b1fea4a3818d909d25c346fc8be5d0a28c8ed9aa
James Thai <jamesthai79@gmail.com>
2026-06-29 05:09:53 -0700

    rudimentary cat done, TODO: process and enable opts for cat

--- cat.c
+++ cat.c
@@ -1,23 +1,94 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char** argv){
-  if(argc < 2 || strcmp(argv[1], "-") == 0) {
-    while(1) {
-      char c = getchar();
+int index_argc = 0;
+void processArguments(int argc, char **argv) {
+  char *opts = "AbeEnstTuv";
+  for (int i = 1; i < argc; i++) {
+    if (argv[i][0] != '-') {
+      continue;
+    }
+    char *ptr = argv[i] + 1;
+    while (*ptr) {
+      if (strchr(opts, *ptr) == NULL) {
+        printf("cat: invalid option -- '%c'\n", *ptr);
+        exit(EXIT_FAILURE);
+      }
+      ptr++;
+    }
+  }
+
+  for (int i = 1; i < argc; i++) {
+    if (argv[i][0] != '-') {
+      continue;
+    }
+    char *ptr = argv[i] + 1;
+    while (*ptr) {
+      switch (*ptr) {
+      case 'A':
+        puts("yay");
+        break;
+      case 'b':
+        break;
+      case 'e':
+        break;
+      case 'E':
+        break;
+      case 's':
+        break;
+      case 't':
+        break;
+      case 'T':
+        break;
+      case 'u':
+        break;
+      case 'v':
+        break;
+      }
+      ptr++;
+    }
+  }
+}
+
+int main(int argc, char **argv) {
+  // processArguments(argc, argv);
+  if (argc < 2 || strcmp(argv[index_argc], "-") == 0) {
+    int c;
+    while (1) {
+      c = getchar();
+      if (c == EOF) {
+        break;
+      }
       putchar(c);
     }
   }
 
-
-  for(int i = 1; i < argc; i++) {
-    FILE* f = fopen(argv[i], "r");
-    if(f == NULL) {
+  for (int i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "-") == 0) {
+      int c;
+      while (1) {
+        c = getchar();
+        if (c == EOF) {
+          break;
+        }
+        putchar(c);
+      }
+      continue;
+    }
+    FILE *f = fopen(argv[i], "r");
+    if (f == NULL) {
       perror("fopen");
       return 1;
     }
-
-
+    fseek(f, 0, SEEK_END);
+    unsigned int bytes = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    int c;
+    while ((c = fgetc(f)) != EOF) {
+      putchar(c);
+    }
     fclose(f);
   }
 
